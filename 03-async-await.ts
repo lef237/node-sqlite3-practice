584157d614e1c571d4cd02381351e5a4f1a3e48c
lef237 <lef237k5@gmail.com>
2023-07-19 18:23:46 +0900

    処理ごとに空行を入れた。close()にもawaitをつけた。

--- 03-async-await.ts
+++ 03-async-await.ts
@@ -9,17 +9,22 @@ const main = async () => {
       db,
       "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)"
     );
+
     const title = "書籍タイトル";
+
     const runResult = await run(db, "INSERT INTO books (title) VALUES (?)", [
       title,
     ]);
     console.log("自動採番された IDは " + runResult.lastID + " です。");
+
     const rows = await all(db, "SELECT id, title FROM books");
     rows.forEach((row) => {
       console.log(row.id + ": " + row.title);
     });
+
     await run(db, "DROP TABLE books");
-    close(db);
+
+    await close(db);
   } catch (err) {
     if (err instanceof Error) {
       console.error(err.message);
