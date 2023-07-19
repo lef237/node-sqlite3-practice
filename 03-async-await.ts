8d76bcfe496bcc7dcbf3c91a5a6d7831d3495a12
lef237 <lef237k5@gmail.com>
2023-07-19 16:59:29 +0900

    libのファイルを反映させて正常系を書き換えた

--- 03-async-await.ts
+++ 03-async-await.ts
@@ -1,61 +1,29 @@
 import sqlite3 from "sqlite3";
+import { run, all, close } from "./lib/db-operation";
 
 const db = new (sqlite3.verbose().Database)(":memory:");
-
-const run = (query: string, params: any = []): Promise<sqlite3.RunResult> => {
-  return new Promise((resolve, reject) => {
-    db.run(
-      query,
-      params,
-      function (this: sqlite3.RunResult, err: Error | null) {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(this);
-        }
-      }
-    );
-  });
-};
-
-const each = (query: string): Promise<void> => {
-  return new Promise((resolve, reject) => {
-    db.each(query, (err: Error | null, row: { id: number; title: string }) => {
-      if (err) {
-        reject(err);
-      } else {
-        console.log(row.id + ": " + row.title);
-        resolve();
-      }
-    });
-  });
-};
-
-const close = (): Promise<void> => {
-  return new Promise((resolve, reject) => {
-    db.close((err: Error | null) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve();
-      }
-    });
-  });
-};
 
 const main = async () => {
   try {
     await run(
+      db,
       "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)"
     );
     const title = "書籍タイトル";
-    const runResult = await run("INSERT INTO books (title) VALUES (?)", title);
+    const runResult = await run(db, "INSERT INTO books (title) VALUES (?)", [
+      title,
+    ]);
     console.log("自動採番された IDは " + runResult.lastID + " です。");
-    await each("SELECT id, title FROM books");
-    await run("DROP TABLE books");
-    await close();
+    const rows = await all(db, "SELECT id, title FROM books");
+    rows.forEach((row) => {
+      console.log(row.id + ": " + row.title);
+    });
+    await run(db, "DROP TABLE books");
+    close(db);
   } catch (err) {
-    console.error((err as Error).message);
+    if (err instanceof Error) {
+      console.error(err.message);
+    }
   }
 };
 
