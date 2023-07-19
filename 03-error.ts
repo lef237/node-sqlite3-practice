1022a54c21db3ea8f882f847211d12195f28c2a4
lef237 <lef237k5@gmail.com>
2023-07-20 08:42:29 +0900

    必要な箇所だけtry~catchするようにした

--- 03-error.ts
+++ 03-error.ts
@@ -4,17 +4,14 @@ import { run, all, close } from "./lib/db-operation";
 const db = new (sqlite3.verbose().Database)(":memory:");
 
 const main = async () => {
-  try {
-    await run(
-      db,
-      "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)"
-    );
+  await run(
+    db,
+    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)"
+  );
 
+  try {
     const title = null;
-    const runResult = await run(db, "INSERT INTO books (title) VALUES (?)", [
-      title,
-    ]);
-    console.log("自動採番された IDは " + runResult.lastID + " です。");
+    await run(db, "INSERT INTO books (title) VALUES (?)", [title]);
   } catch (err) {
     if (err instanceof Error) {
       console.error(err.message);
@@ -29,15 +26,9 @@ const main = async () => {
     }
   }
 
-  try {
-    await run(db, "DROP TABLE books");
+  await run(db, "DROP TABLE books");
 
-    await close(db);
-  } catch (err) {
-    if (err instanceof Error) {
-      console.error(err.message);
-    }
-  }
+  await close(db);
 };
 
 main();
